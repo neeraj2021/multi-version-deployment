45d6a7b8192de064f4ef4f12c5db294924376127
Neeraj Kumar <neeraj@flick2know.com>
2024-07-18 22:57:07 +0530

    chore: random commit

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const app = express();
 const pJson = require("./package.json");
 
+const PORT = 3000;
+
 app.get("/", (req, res) => {
-  console.log(`Request received in '/' ${new Date()} from ${req.ip}, Version: ${pJson.version}`);
+  console.log(
+    `Request received in '/' ${new Date()} from ${req.ip}, Version: ${
+      pJson.version
+    }`
+  );
   res.send("Hello World!");
 });
 
 app.get("/version", (req, res) => {
-  console.log(`Request received in '/version' ${new Date()} from ${req.ip}, Version: ${pJson.version}`);
+  console.log(
+    `Request received in '/version' ${new Date()} from ${req.ip}, Version: ${
+      pJson.version
+    }`
+  );
   res.send("Project version: " + pJson.version);
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
